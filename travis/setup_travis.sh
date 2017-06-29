10e6e4db7d7f96ee7b9125f84ac711479abd9f45
Philip Lombardi <plombardi@datawire.io>
2017-06-29 14:02:39 -0400

    Add encrypted kubeconfig

--- travis/setup_travis.sh
+++ travis/setup_travis.sh
@@ -2,3 +2,6 @@
 set -euxo pipefail
 
 mkdir -p ${TOOLS_DIR:?"ERROR: Variable TOOLS_DIR not set"}
+mkdir -p ${HOME}/.kube
+
+openssl aes-256-cbc -K $encrypted_83ffded4ef39_key -iv $encrypted_83ffded4ef39_iv -in kubeconfig.enc -out ${HOME}/.kube/config -d
