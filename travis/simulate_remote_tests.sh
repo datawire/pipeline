db499b8706905c92bbf2611d3a8db9c954cbb2af
Philip Lombardi <plombardi@datawire.io>
2017-06-29 16:11:17 -0400

    retry with curl

--- travis/simulate_remote_tests.sh
+++ travis/simulate_remote_tests.sh
@@ -2,4 +2,13 @@
 set -euxo pipefail
 
 ps aux | grep kubectl
-curl -4 -v "http://127.0.0.1:5000/"
+curl -4 \
+     -v \
+     --connect-timeout 5 \
+     --max-time 10 \
+     --retry 5 \
+     --retry-delay 0 \
+     --retry-max-time 40 \
+     "http://127.0.0.1:5000/"
+
+
