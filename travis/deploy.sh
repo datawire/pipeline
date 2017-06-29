902096a26de51c4a705ce29fc289ab47339e8578
Philip Lombardi <plombardi@datawire.io>
2017-06-29 14:58:42 -0400

    add committing manifests to pipeline

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -5,3 +5,12 @@ kubectl apply -f kubernetes/staging
 
 kubectl config use-context production
 kubectl apply -f kubernetes/production
+
+git add kubernetes/*
+
+NOW=$(TZ=America/New_York date)
+
+git commit -m "kubernetes manifests: $NOW [ci skip]"
+
+echo "Pushing changes to upstream master"
+git push upstream master
