105e48e63fc0cf7680e1fbebd7f12b174455866f
Philip Lombardi <plombardi@datawire.io>
2017-06-29 16:15:26 -0400

    ...

--- travis/simulate_remote_tests.sh
+++ travis/simulate_remote_tests.sh
@@ -2,13 +2,12 @@
 set -euxo pipefail
 
 ps aux | grep kubectl
+sleep 10s
+ps aux | grep kubectl
 curl -4 \
      -v \
-     --connect-timeout 5 \
-     --max-time 10 \
-     --retry 5 \
-     --retry-delay 0 \
-     --retry-max-time 40 \
-     "http://127.0.0.1:5000/"
+      "http://127.0.0.1:5000/"
+
+
 
 
